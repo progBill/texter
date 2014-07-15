218b4047931b8d88fafcfc38376f16dcff3642fb
Bill <billebeling@gmail.com>
2014-07-14 20:30:22 -0400

    initial commit of word / text objects

---
+++ text.py
@@ -0,0 +1,41 @@
+
+
+class Word:
+
+    def __init__(self, word):
+        self.word=word
+        self.qual={}
+
+
+    def set_quality(self, key, val):
+        self.qual[key]=val
+
+class Text:
+
+    def __init__(self, unbroken_text, num_chunk=None, len_chunk=None):
+        self.words = [new Word(x) for x in unbroken_text.split()]
+        self.chunks=[]
+        self.unigram={}
+
+        # ordered list of words
+        if num_chunk:
+            #TODO: I think below will leave some words off 
+            n=self.num_chunk/len(self.num_words)
+            for i in xrange(0, len(num_chunks), n):
+                self.chunks.append(self.words[i:i+n])
+        elif len_chunk:
+            n=self.len_chunk/len(self.words)
+            for i in xrange(0, len(self.words), len_chunk):
+                self.chunks=[x for x in self.unbroken_text[i:i+n]]
+
+
+        # dict of token:num_appearing
+        for x in self.chunks:
+            if x in self.unigram.keys():
+                self.unigram[x]+=1
+            else:
+                self.unigram[x]=1
+
+
+
+

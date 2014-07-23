2bb43c2e1cab21cf7523ac6769603e3ccd42b7f1
Bill <billebeling@gmail.com>
2014-07-23 18:48:13 -0400

    Added a text intake, fixed boneheaded errors

--- text.py
+++ text.py
@@ -1,11 +1,9 @@
 
 
 class Word:
-
     def __init__(self, word):
         self.word=word
         self.qual={}
-
 
     def set_quality(self, key, val):
         self.qual[key]=val
@@ -13,17 +11,17 @@ class Word:
 class Text:
 
     def __init__(self, unbroken_text, num_chunk=None, len_chunk=None):
-        self.words = [new Word(x) for x in unbroken_text.split()]
+        self.words = [Word(x) for x in unbroken_text.split()]
         self.chunks=[]
         self.unigram={}
 
         # ordered list of words
         if num_chunk:
             #TODO: I think below will leave some words off 
-            n=self.len(self.words)/num_chunk
+            n=len(self.words)/num_chunk
             for i in range(n):
                 self.chunks=[x for x in self.words[i:i+n]]
-       elif len_chunk:
+        elif len_chunk:
             n=self.len_chunk/len(self.words)
             for i in range(n):
                 self.chunks=[x for x in self.words[i:i+n]]
@@ -34,7 +32,5 @@ class Text:
                 self.unigram[x]+=1
             else:
                 self.unigram[x]=1
-
-
 
 
